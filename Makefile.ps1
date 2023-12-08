17c377c682832a4366e4512efe15a05b1db0fefa
Moshiur Howlader <97899093+moshiurH@users.noreply.github.com>
2023-12-08 12:22:50 -0500

    Update Makefile.ps1
    Updated as per the PR review.

--- Makefile.ps1
+++ Makefile.ps1
@@ -1,9 +1,39 @@
 # Makefile.ps1
+# Windows alternative to a makefile to allow easier editing of the hugo based documentation on Windows
+
+# Show help message
+function Show-Help {
+    Write-Output "help                  Show this help message",
+                 "install               Install bctl on your system",
+                 "docs                  Run the documentation website locally"
+}
+
+# Function to perform actions based on provided flag
+function Execute-Target {
+    param (
+        [string]$target
+    )
+
+    switch ($target) {
+        "install" {
+            Write-Output "Installing bctl on Windows not supported!"
+            #.\scripts\install.sh
+        }
+        "docs" {
+            hugo server --source website/ --disableFastRender
+        }
+        "help" {
+            Show-Help
+        }
+        default {
+            Write-Output "Unknown target: $target"
+        }
+    }
+}
 
 # Show help message
 if ($args.Count -eq 0) {
-    Write-Output "Insufficient option(s) provided!",
-                  "Should be: .\Makefile.ps1 [option]"
+    Show-Help
     exit
 }
 
@@ -11,16 +41,4 @@ if ($args.Count -eq 0) {
 $target = $args[0]
 
 # Execute targets
-switch ($target) {
-    "install" {
-        Write-Output "Installing bctl on Windows not supported!"
-        #.\scripts\install.ps1
-    }
-    "docs" {
-        Write-Output "Running the documentation website locally"
-        hugo server --source website/ --disableFastRender
-    }
-    default {
-        Write-Output "Unknown target: $target"
-    }
-}
+Execute-Target -target $target
